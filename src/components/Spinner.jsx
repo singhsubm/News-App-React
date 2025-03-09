433297b9e1ca0f7768ae6c2807574f95f263caf9
True-Beast ‎ <singhsubm24@gamil.com>
2025-03-10 03:14:01 +0530

    first commit

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,6 +3,12 @@ import './Spinner.css'
 
 export default function Spinner() {
   return (
-    <span className="loader"></span>
+    <div className='w-full h-[60vh] flex items-center justify-center flex-col'>
+
+      <div className="loader"></div>
+      <div>
+        Please Wait...
+      </div>
+    </div>
   )
 }
